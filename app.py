4fdd49d29565a497206d9eb2af997a9204269e15
Vahagn Altunyan <altunyanv@gmail.com>
2024-08-13 23:26:16 +0400

    Working version on server upload

--- app.py
+++ app.py
@@ -1,6 +1,40 @@
-from flask import Flask
+import os
+
+from flask import Flask, send_from_directory, render_template, request
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Welcome"
+# Set the folder for uploaded files
+UPLOAD_FOLDER = 'uploads/'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return "No file part"
+    file = request.files['file']
+    if file.filename == '':
+        return "No selected file"
+    if file:
+        filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+        file.save(filepath)
+        return f"File uploaded successfully: {file.filename}"
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+if __name__ == '__main__':
+    app.run(debug=True)
